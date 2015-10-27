fad76b7326fdd0b5f8b46305a3e268f7868f272a
Meiyah <jenskoeleman@gmail.com>
2015-10-27 13:21:56 +0100

    Defined the code

--- QR Code/QRcode.py
+++ QR Code/QRcode.py
@@ -1,13 +1,17 @@
 #Importeer de qrcode module en definier qr als qrcode.QRcode
 import qrcode
-qr = qrcode.QRCode()
 
-#Hier wordt de data toegevoegd
-qr.add_data('Kaartnummer= 100 ,E-mail= abc@abc.com, code=8465bxfgeu')
-qr.make(fit=True)
+def QRcode():
+    qr = qrcode.QRCode()
 
-#img bevat een PIL.image.image object
-img = qr.make_image()
+    #Hier wordt de data toegevoegd
+    qr.add_data('Kaartnummer= 1023 ,E-mail= abc@abc.com, code=8465bxfgeu')
+    qr.make(fit=True)
 
-#Save the Code
-img.save('test2.png')
+    #img bevat een PIL.image.image object
+    img = qr.make_image()
+
+    #Save the Code
+    img.save('qrcode.png')
+
+QRcode()
